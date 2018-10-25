c290ed8643cdee4cb5f9c110bf150b0833e8c669
Renato Herebia <rherebia@gmail.com>
2018-10-25 17:21:01 -0400

    Parametrizando url de login e token; tratando erro no login

--- auth-server/src/main/java/br/com/rbh/authserver/config/security/RestAuthenticationProvider.java
+++ auth-server/src/main/java/br/com/rbh/authserver/config/security/RestAuthenticationProvider.java
@@ -1,63 +1,97 @@
 package br.com.rbh.authserver.config.security;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringWriter;
+import java.net.URI;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
+import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Component("rest")
 public class RestAuthenticationProvider implements AuthenticationProvider {
+	
+	@Value("${authentication.authToken}")
+	private String authToken;
+	
+	@Value("${authentication.loginURI}")
+	private String loginURI;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		try {
-			String login = authentication.getName();
-	        String senha = authentication.getCredentials().toString();
-	        
-	    	RestTemplate template = new RestTemplate();
-    	
-    		HttpHeaders headers = new HttpHeaders();
-    		headers.add("X-AUTH-TOKEN", "c05e2183af5701686607c28a6839573f");
-    		headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-    		
-    		HttpEntity<String> request = new HttpEntity<String>(new ObjectMapper().writeValueAsString(new LoginRequest(login, senha)), headers);
-    		
-			ResponseEntity<String> response = template.postForEntity("https://sistemas5.ufms.br/passaporte-ws/api/ad_users", 
-					request, String.class);
+		String login = authentication.getName();
+        String senha = authentication.getCredentials().toString();
+	
+		RequestEntity<LoginRequest> requestEntity = RequestEntity
+				.post(URI.create(loginURI))
+				.accept(MediaType.APPLICATION_JSON)
+				.header("X-AUTH-TOKEN", authToken)
+				.body(new LoginRequest(login, senha));
+		
+		RestTemplate template = new RestTemplate();
+		template.setErrorHandler(new ResponseErrorHandler() {
 			
-			if (response.getStatusCode() == HttpStatus.OK) {
-				return new UsernamePasswordAuthenticationToken(login, senha, new ArrayList<>());
-			} else {
-				throw new AuthenticationCredentialsNotFoundException("Autenticação falhou para usuário " + login);
+			@Override
+			public boolean hasError(ClientHttpResponse response) throws IOException {
+				return !response.getStatusCode().equals(HttpStatus.OK);
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new AuthenticationCredentialsNotFoundException("Problema durante a autenticação");
-		}
+			
+			@Override
+			public void handleError(ClientHttpResponse response) throws IOException {
+				if (response.getStatusCode().equals(HttpStatus.BAD_REQUEST)) {
+					StringWriter writer = new StringWriter();
+					IOUtils.copy(response.getBody(), writer, Charset.defaultCharset());
+					String body = writer.toString();
+					
+					DefaultResponse defaultResponse = new ObjectMapper().readValue(body, DefaultResponse.class);
+					
+					throw new AuthenticationCredentialsNotFoundException(defaultResponse.getMsg());
+				}
+			}
+		});
+		
+		template.exchange(requestEntity, String.class);
+		
+		return new UsernamePasswordAuthenticationToken(login, senha, new ArrayList<>());
 	}
 
 	@Override
 	public boolean supports(Class<?> authentication) {
 		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
+	
+	static class DefaultResponse {
+		private String msg;
+
+		public String getMsg() {
+			return msg;
+		}
+
+		public void setMsg(String msg) {
+			this.msg = msg;
+		} 
+	}
 
 	class LoginRequest implements Serializable {
 		private static final long serialVersionUID = 1L;
 		
-		String login;
-		String senha;
+		private String login;
+		private String senha;
 		
 		LoginRequest(String login, String senha) {
 			this.login = login;
